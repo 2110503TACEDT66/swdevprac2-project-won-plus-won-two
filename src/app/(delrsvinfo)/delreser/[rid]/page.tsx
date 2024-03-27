bc73b3d2180cd2a2c48a081198a00612c0bef9f5
Assari Y <143721599+Assariii@users.noreply.github.com>
2024-03-27 17:19:21 +0700

    Update page.tsx

--- src/app/(delrsvinfo)/delreser/[rid]/page.tsx
+++ src/app/(delrsvinfo)/delreser/[rid]/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { authOptions } from "@/libs/auth";
 import getUserProfile from "@/libs/getUserProfile"
 import { redirect } from "next/navigation"
 import getReservationID from "@/libs/getReservationID"
