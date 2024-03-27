c416748a8c9fce0b21dcd62958677d3dc7a4f053
Assari Y <143721599+Assariii@users.noreply.github.com>
2024-03-27 17:44:26 +0700

    Update ReservationCatalog.tsx

--- src/components/ReservationCatalog.tsx
+++ src/components/ReservationCatalog.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link"
 import ReservationCard from "./ReservationCard"
+import { RestaurantJson, ReservationItem, ReservationJson } from "../../interfaces"
 
-export default async function ReservationCatalog({carJson}:{carJson:object}) {
+export default async function ReservationCatalog({carJson}:{carJson:ReservationJson}) {
     const carJsonReady = await carJson
     return (
         <>
         {carJsonReady.count} Reservation
         <div style={{margin:"20px", display:"flex", flexDirection:"row" , flexWrap:"wrap", justifyContent:"space-around",alignContent:"space-around"}}>
                 {
-                    carJsonReady.data.map((carItem:object)=>(
+                    carJsonReady.data.map((carItem:ReservationItem)=>(
                         <div className="w-[100%] sm:w-[50%] md:w-[30%] lg:w-[25%] p-2 sm:p-4 md:p-4 lg:p-8">   
                             <ReservationCard rsvDate={carItem.rsvDate} user={carItem._id}  restaurant={carItem.restaurant} createdAt={carItem.createdAt} rsvID={carItem._id}/>          
                         </div>
